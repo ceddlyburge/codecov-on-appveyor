a2f9f7f86f82c10bb2960fc652a567cb6385fefd
CeddlyBurge <CeddlyBurge@gmail.com>
2017-01-31 08:33:36 +0000

    Add Tests project

---
+++ Tests/Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Fact]
+        void LastNameCommaFirstName()
+        {
+            const string FIRST_NAME = "cedd";
+            const string LAST_NAME = "cedd";
+
+            var actual = new NameFormatter.FullName(FIRST_NAME, LAST_NAME);
+
+            Assert.Equal($"{LAST_NAME}, {FIRST_NAME}", actual);
+        }
+    }
+}
