3705f2776c3952bb558af464f01fb593f9e25f91
CeddlyBurge <CeddlyBurge@gmail.com>
2017-02-03 08:21:03 +0000

    Add system under test

---
+++ SystemUnderTest/NameFormatter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SystemUnderTest
+{
+    public class NameFormatter
+    {
+        public string FullName(string firstName, string lastName)
+        {
+            return $"{lastName}, {firstName}";
+        }
+    }
+}
--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SystemUnderTest;
 using Xunit;
 
 namespace Tests
@@ -12,9 +13,9 @@ namespace Tests
         void LastNameCommaFirstName()
         {
             const string FIRST_NAME = "cedd";
-            const string LAST_NAME = "cedd";
+            const string LAST_NAME = "burge";
 
-            var actual = new NameFormatter.FullName(FIRST_NAME, LAST_NAME);
+            var actual = new NameFormatter().FullName(firstName: FIRST_NAME, lastName: LAST_NAME);
 
             Assert.Equal($"{LAST_NAME}, {FIRST_NAME}", actual);
         }
